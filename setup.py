2a328e0c9df755e51f307d83f6512761bdcc7ad0
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-03-20 13:39:17 +0300

    setuptools for making pkg

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+PACKAGE = "fpe"
+
+
+setup(
+    name=PACKAGE,
+    version=__import__(PACKAGE).__version__,
+    packages=find_packages(exclude=["tests.*", "tests", "*.md"]),
+    description="Functional programming tools",
+    long_description=open("README.txt").read(),
+    test_suite="tests",
+    author="Constantine Kormashev",
+    author_email="constantine.kormashev@gmail.com",
+    license="Mozilla Public License 2.0",
+    url="https://github.com/0xck/fpelements",
+    zip_safe=False
+)
