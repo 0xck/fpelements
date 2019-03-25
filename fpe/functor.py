e03104e8101ef8fff57a4561d4d3fe1e84c1672f
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-03-25 19:12:01 +0300

    functor abstraction module

---
+++ fpe/functor.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any, Callable
+
+from fpe.asserts import AssertNotCallable, AssertWrongArgumentType
+from fpe.functions import curry
+
+
+class AbstractFunctor(metaclass=ABCMeta):
+
+    @abstractmethod
+    def fmap(self, func: Callable) -> Any:
+        """
+        fmap function for mapping values with functions
+
+        Borrowed from fmap :: (a -> b) -> f a -> f b
+
+        Note.
+            fmap must satisfy the following laws:
+                fmap id  ==  id
+                fmap (f . g)  ==  fmap f . fmap g
+
+            This is not possible to satisfy these laws in some automatical way,
+            implementation should be performed manually.
+        """
+        pass
+
+
+@curry
+def fmap(func: Callable, instance) -> Any:
+
+    # only callable
+    assert callable(func), AssertNotCallable()
+    # only Functor
+    assert isinstance(instance, AbstractFunctor), AssertWrongArgumentType(
+        "AbstractFunctor")
+
+    return instance.fmap(func)
