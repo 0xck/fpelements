a74698bf85919ddd1b5bc6c277676665b51f8017
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-03-25 19:10:23 +0300

    fpe.base was added as module for importand functions

---
+++ fpe/base.py
@@ -0,0 +1,27 @@
+from typing import Any, Callable
+
+from fpe.asserts import AssertNotCallable
+
+
+def flip(func: Callable) -> Callable:
+    """
+    Decorator swaps arguments provided to decoreted function
+
+    Thus flip(f)(x, y) == f(y, x)
+
+    Borrowed from flip :: (a -> b -> c) -> b -> a -> c
+    """
+
+    # only callable
+    assert callable(func), AssertNotCallable()
+
+    def flipped(first: Any, second: Any) -> Any:
+        """
+        Thus f(x, y) -> f(y, x)
+        """
+
+        return func(second, first)
+
+    flipped.__name__ = "flipped {}".format(getattr(func, "__name__", "Unknown"))
+
+    return flipped
