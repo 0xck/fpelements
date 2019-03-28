f6e02cf6aa9cced27336a0196c48e699cdfb7b5a
Constantine Kormashev <constantine.kormashev@gmail.com>
2019-03-28 23:37:14 +0300

    added odd and even functions doc, spell, typo work

--- fpe/base.py
+++ fpe/base.py
@@ -1,11 +1,10 @@
 from typing import Any, Callable
 
-from fpe.asserts import AssertNotCallable
+from fpe.asserts import AssertNotCallable, AssertWrongArgumentType
 
 
-def flip(func: Callable) -> Callable:
-    """
-    Decorator swaps arguments provided to decorated function.
+def flip(func: Callable[[Any, Any], Any]) -> Callable:
+    """Decorator swaps arguments provided to decorated function.
 
     Thus flip(f)(x, y) == f(y, x)
 
@@ -16,14 +15,34 @@ def flip(func: Callable) -> Callable:
     assert callable(func), AssertNotCallable()
 
     def flipped(first: Any, second: Any) -> Any:
-        """
-        Swapping provided arguments to given function.
+        """Swapping provided arguments to given function.
 
         Thus f(x, y) -> f(y, x)
         """
 
         return func(second, first)
 
-    flipped.__name__ = "flipped <{}>".format(getattr(func, "__name__", "Unknown"))
+    flipped.__name__ = getattr(func, "__name__", "Unknown")
+
+    if hasattr(func, "__doc__"):
+        flipped.__doc__ = getattr(func, "__doc__")
 
     return flipped
+
+
+def even(num: int) -> bool:
+    "Return True if given num is even, otherwise False"
+
+    # only int
+    assert isinstance(num, int), AssertWrongArgumentType("int")
+
+    return not (num % 2)
+
+
+def odd(num: int) -> bool:
+    "Return True if given num is odd, otherwise False"
+
+    # only int
+    assert isinstance(num, int), AssertWrongArgumentType("int")
+
+    return bool(num % 2)
